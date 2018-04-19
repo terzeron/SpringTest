dd8efc658d8cc93f69550607795cf5f5302cb240
Youngil Cho <terzeron@gmail.com>
2018-04-20 00:21:20 +0900

    add a module of AOP example

---
+++ AOPTest1/src/main/java/BusinessLogic.java
@@ -0,0 +1,7 @@
+public class BusinessLogic implements IBusinessLogic
+{
+    public void foo()
+    {
+        System.out.println("Inside BusinessLogic.foo()");
+    }
+}
---
+++ AOPTest1/src/main/java/IBusinessLogic.java
@@ -0,0 +1,4 @@
+public interface IBusinessLogic
+{
+    void foo();
+}
