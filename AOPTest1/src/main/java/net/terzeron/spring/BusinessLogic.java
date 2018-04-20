d2317021cd807a9cbca71240675ff15844c46262
Young-il Cho <youngilcho@nhnent.com>
2018-04-20 14:14:46 +0900

    remove a module

--- AOPTest1/src/main/java/net/terzeron/spring/BusinessLogic.java
+++
@@ -1,9 +0,0 @@
-package net.terzeron.spring;
-
-public class BusinessLogic implements IBusinessLogic
-{
-    public void foo()
-    {
-        System.out.println("Inside BusinessLogic.foo()");
-    }
-}
--- AOPTest1/src/main/java/net/terzeron/spring/IBusinessLogic.java
+++
@@ -1,6 +0,0 @@
-package net.terzeron.spring;
-
-public interface IBusinessLogic
-{
-    void foo();
-}
