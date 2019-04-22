b93e71e0eca873f381a0116c2eafa9c29356e851
Young-il Cho <youngilcho@nhnent.com>
2019-04-22 19:19:08 +0900

    add some modules

--- LazyLoadingTest/src/main/java/com/terzeron/spring/LazyInit.java
+++ LazyLoadingTest/src/main/java/com/terzeron/spring/LazyInit.java
@@ -1,4 +1,12 @@
 package com.terzeron.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LazyInit {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
+        System.out.println("after initialization");
+        applicationContext.getBean("addressBean");
+    }
 }
--- LoadingWithBeanFactory/src/main/java/com/terzeron/spring/LoadingWithBeanFactory.java
+++ LoadingWithBeanFactory/src/main/java/com/terzeron/spring/LoadingWithBeanFactory.java
@@ -1,4 +1,21 @@
 package com.terzeron.spring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+
 public class LoadingWithBeanFactory {
+    public static void main(String[] args) {
+        System.out.println(new File(".").getAbsolutePath());
+        Resource xmlResource = new FileSystemResource("target/classes/applicationContext.xml");
+        BeanFactory factory = new XmlBeanFactory(xmlResource);
+        Employee employeeBean = (Employee) factory.getBean("employeeBean");
+        System.out.println(employeeBean.getName());
+        System.out.println(employeeBean.getAddress().getStreet());
+        System.out.println(employeeBean.getAddress().getCity());
+        System.out.println(employeeBean.getAddress().getPincode());
+    }
 }
